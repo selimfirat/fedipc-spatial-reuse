abc3cecb54c6f6855f9f3bc33f7a2b731e5fbe46
selimfirat <yilmazselimfirat@gmail.com>
2021-10-15 18:51:09 +0100

    Fix fedprox grad issue

--- federated_trainers/fed_prox_trainer.py
+++ federated_trainers/fed_prox_trainer.py
@@ -9,8 +9,15 @@ class FedProxTrainer(FedAvgTrainer):
         super(FedProxTrainer, self).__init__(**cfg)
 
     def proximal_term(self, current_state_dict, original_state_dict):
+        diff_sum = 0.0
+        for key in original_state_dict.keys():
+            modules = key.split(".")
+            if len(modules) == 3:
+                diff_sum += torch.sum(( getattr(getattr(self.model, modules[0])[int(modules[1])], modules[2]) )**2)
+            elif len(modules) == 2:
+                diff_sum += torch.sum(( getattr(getattr(self.model, modules[0]), modules[1]) )**2)
 
-        return 0.5 * self.cfg["mu"] * torch.sum(torch.stack([torch.sum((current_state_dict[key] - original_state_dict[key])**2) for key in current_state_dict.keys()]))
+        return 0.5 * self.cfg["mu"] * diff_sum
 
     def loss(self, y_pred, y, current_state_dict, original_state_dict):
 
