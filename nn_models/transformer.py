825243b8acf22de32bb7d5c27f03cfda187be0f1
kerem <keremozfatura@me.com>
2021-10-26 20:54:01 +0300

    Hybrid L1-mse loss

--- nn_models/transformer.py
+++ nn_models/transformer.py
@@ -4,13 +4,17 @@ from torch.nn import LayerNorm
 
 class Transformer(nn.Module):
 
-    def __init__(self,output_size, nhead,num_layers,dropout,device,d_model=8, **params):
+    def __init__(self,output_size, nhead,num_layers,dropout,device,scenario, **params):
         super(Transformer, self).__init__()
+        if scenario ==1:
+            self.d_model = 8
+        elif scenario ==2:
+            self.d_model = 14
         self.output_size = output_size
-        self.encoder_layer = nn.TransformerEncoderLayer(dropout=dropout,d_model=d_model, nhead=nhead,batch_first=True,device=device)
-        encoder_norm = LayerNorm(d_model)
-        self.transfor_encode = nn.TransformerEncoder(self.encoder_layer, num_layers,norm=encoder_norm)
-        self.linear = nn.Linear(d_model, self.output_size)
+        self.encoder_layer = nn.TransformerEncoderLayer(dropout=dropout,d_model=self.d_model, nhead=nhead,batch_first=True,device=device)
+        self.encoder_norm = LayerNorm(self.d_model)
+        self.transfor_encode = nn.TransformerEncoder(self.encoder_layer, num_layers,norm=self.encoder_norm)
+        self.linear = nn.Linear(self.d_model, self.output_size)
 
     def forward(self,x):
         x= x.unsqueeze(0)
